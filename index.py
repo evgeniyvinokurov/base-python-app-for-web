ee980d46bde3e8dc04401ade47b9af23987ddb07
evgenii <evgenii@localhost.localdomain>
2025-07-23 15:23:09 +0300

    index project

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from bottle import default_app, run
 
 import router
+from projects.index.codes.index import router
 
 application = default_app()
 application.run()
---
+++ projects/index/codes/index/router.py
@@ -0,0 +1,9 @@
+from bottle import redirect, route, view, post, request, get, response, auth_basic
+from bottle import static_file
+
+@route('/')
+@view('index')
+def feedback(name='World'):
+    param = {}
+    return param   
+
