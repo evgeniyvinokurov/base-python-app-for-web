2b72584b03e670bb187c615008f65f13fcf1e07a
Evgeniy Vinokurov <evgeniy.vinokuroff@yandex.ru>
2026-01-14 17:11:53 +0300

    index projects site merging rebuild and complex design refactoring

--- projects/index/codes/index/router.py
+++ projects/index/codes/index/router.py
@@ -1,9 +1,30 @@
 from bottle import redirect, route, view, post, request, get, response, auth_basic
 from bottle import static_file
 
-@route('/')
+@route('/python/')
 @view('python')
 def feedback(name='World'):
     param = {}
     return param   
 
+@route('/')
+@view('index')
+def feedback(name='World'):
+    param = {}
+    return param   
+
+@route('/more')
+@view('more')
+def feedback(name='World'):
+    param = {}
+    return param   
+
+@route('/faq/')
+@view('faq')
+def feedback(name='World'):
+    param = {}
+    return param   
+
+@route('/static/:path#.+#', name='static')
+def static(path):
+    return static_file(path, root='static')
